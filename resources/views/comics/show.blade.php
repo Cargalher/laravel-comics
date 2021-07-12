493ec5eef6cd371c6d41c91a9f41be0ef00ec1da
Cargalher <cargalher@gmail.com>
2021-07-12 13:07:33 +0200

    single comic page finished

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -12,55 +12,55 @@
 
 <div class="comic_description">
     <div class="bg_brand">
-
+    <img src="{{$comic['thumb'] }}" alt="" class="img_show">
     </div>
-    <section class="container bg_comics">
-    <div class="comics">
-        
-        <div class="comic_card_show">
-
-            <img src="{{$comic['thumb'] }}" alt="" class="img_show">
-
-            <div class="container_comic_description">
-                <div class="left_description">
-                    <h2>{{$comic['title']}}</h2>
-                    <div>
-                        <p 
-                        class="price">US.Price: $ {{$comic['price']}}
-                        <span>Available</span>
-                        <span>Check Availibility</span>
-                    </p>
-                        
-                    </div>
-                    
-                    <p class="desc"> {{$comic['description']}}</p>
-                </div>
-                <div class="img_description">
-                    <img src="{{asset('img/adv.jpg')}}" alt="logo" class="adv_img">
-                </div>
-            </div>
+    <section class=" bg_comics">
+        <div class="comics container_dimension">
             
+            <div class="comic_card_show">
+
+                
+
+                <div class="container_comic_description">
+                    <div class="left_description">
+                        <h2>{{$comic['title']}}</h2>
+                        <div>
+                            <p 
+                            class="price">US.Price: $ {{$comic['price']}}
+                            <span>Available</span>
+                            <span>Check Availibility</span>
+                        </p>
+                            
+                        </div>
+                        
+                        <p class="desc"> {{$comic['description']}}</p>
+                    </div>
+                    <div class="img_description">
+                        <span style="text-transform: uppercase; align-text:right;">Advertisement</span>
+                        <img src="{{asset('img/adv.jpg')}}" alt="logo" class="adv_img">
+                    </div>
+                </div>
+                
 
 
+            </div>
         </div>
-    </div>
     
- 
-</section>
 <div class="artists">
-        <div class="talent">
+    <div class="container_dimension">
+        <div class="talent ">
             <h3>Talent</h3>
             <div class="border_tp">Art by:
                  <p>
                 
                 
-                @if($comic['artists'])
-                @forelse($comic['artists'] as $artist)
-                <a href="#" style="color:#0282F9;text-decoration:none">{{$artist}}</a>
-                @empty
-                <a href="#">No artists to show</a>
-                @endforelse
-                @endif
+                    @if($comic['artists'])
+                    @forelse($comic['artists'] as $artist)
+                    <a href="#" style="color:#0282F9;text-decoration:none">{{$artist}}</a>
+                    @empty
+                    <a href="#">No artists to show</a>
+                    @endforelse
+                    @endif
                 </p>  
             
             </div> 
@@ -68,15 +68,15 @@
             <div  class="border_tp">Writen by:
                 <p>
                      
-                @if($comic['writers'])
-                @forelse($comic['writers'] as $writer)
-                <a href="#" style="color:#0282F9;text-decoration:none">{{$writer}}</a>
-                @empty
-                <a href="#">No writers to show</a>
-                @endforelse
-                @endif
+                    @if($comic['writers'])
+                    @forelse($comic['writers'] as $writer)
+                    <a href="#" style="color:#0282F9;text-decoration:none">{{$writer}}</a>
+                    @empty
+                    <a href="#">No writers to show</a>
+                    @endforelse
+                    @endif
                 </p>  
-                </p> 
+               
             </div>
         </div>  
 
@@ -86,14 +86,19 @@
             <div  class="border_tp">US.Price: <span>{{$comic['price']}}</span></div>
             <div class="border_tp">On Sale Date: <span>{{$comic['sale_date']}}</span></div>
         </div>  
+    </div>
+        
         
 </div>
-<div class="digital_shop">
-    <div class="side_border">Digital comics</div>
-    <div class="side_border">Shop DC</div>
-    <div class="side_border">Comic shop locator</div>
-    <div class="side_border">Subscriptions</div>
+<div class="digital_shop ">
+    <div class="container_dimension">
+        <div class="side_border">Digital comics</div>
+        <div class="side_border">Shop DC</div>
+        <div class="side_border">Comic shop locator</div>
+        <div class="side_border">Subscriptions</div>
+    </div>
+    
 </div>
-
+</section>
 
 @endsection
